809d23e1e858079a30343ed4f5ff4399b0edc6f8
Dimple Rana <dimpleranaji@gmail.com>
2024-12-08 19:03:51 +0530

    Add route.ts

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,52 @@
+import { connect } from "@/dbConfig/dbConnect";
+
+import User from "@/models/userModel.js";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import { sendEmail } from "@/helpers/mailer";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    //Validation
+    console.log(reqBody);
+    if (!username || !email || !password) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    //Send Verification Email
+    await sendEmail({ email, emailType: "Verify", userId: savedUser._id });
+
+    return NextResponse.json({
+      message: "user registered successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
