a2d5f62c4902cc0b8b0e517e60fd46698bdfa94a
Dimple Rana <dimpleranaji@gmail.com>
2024-12-12 22:58:05 +0530

    Add Signup  page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 export default function page() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("./login");
+    } catch (error: any) {
+      console.log("Signup Failed");
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
-    <div>page</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "Signup"}</h1>
+      <hr />
+
+      <label htmlFor="username">Username</label>
+      <input
+        className="p-2  text-black"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2  text-black"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 text-black"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button className="btn p-2 bg-green-400 mt-3" onClick={onSignup}>
+        {buttonDisabled ? "Fill the Form" : "SignUp"}
+      </button>
+      <Link href={"/login"}>Login Page</Link>
+    </div>
+  );
 }
